60bea53985b38915344cc47baa0a0adeab19238e
Caterina Nahler <caterina@nahler.eu>
2019-02-09 13:26:38 +0100

    Added the function recursion

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-      var botui = new BotUI('hello-world');
+var botui = new BotUI('hello-world');
 
+function deal_with_answer(answer)
+{
+    botui.message.add({
+        content: 'You said: ' + answer.value
+      });
+}
+
+function loop_conversation()
+{
+    botui.action.text({
+      action: {
+        placeholder: 'Enter your text here'
+      }
+    }).then(deal_with_answer)
+    .then(loop_conversation);
+}
+
+function main()
+{
       botui.message.add({
         content: 'Hello World from bot!'
-      }).then(function () { // wait till previous message has been shown.
-
-        botui.message.add({
-          delay: 1000,
-          human: true,
-          content: 'Hello World from human!'
-        });
-      });
+      }).then(loop_conversation);
+}
+      
+main(); 
